5ce224d6eeef6d8d6c9218dd77d7bb16a7ffb550
A1-mamun <1154almamun@gmail.com>
2025-09-19 04:31:35 +0600

    refactor: improve parameter destructuring in ProductDetailsPage component

--- src/app/(CommonLayout)/products/[id]/page.tsx
+++ src/app/(CommonLayout)/products/[id]/page.tsx
@@ -1,8 +1,11 @@
 import SingleProduct from "@/components/modules/product/SingleProduct";
 
-const ProductDetailsPage = async ({ params }: { params: { id: string } }) => {
+const ProductDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
   const { id } = await params;
-
   return <SingleProduct id={id} />;
 };
 
